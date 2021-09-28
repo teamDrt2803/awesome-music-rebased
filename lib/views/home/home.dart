e390c81344fbaa925bb21d7bf505b3b87fdd4e70
= <=>
2021-09-29 03:40:49 +0530

    Song downloading capabilities working fine for IOS

--- lib/views/home/home.dart
+++ lib/views/home/home.dart
@@ -2,6 +2,7 @@ import 'package:awesome_music_rebased/controllers/app_controllers.dart';
 import 'package:awesome_music_rebased/controllers/songs_controller.dart';
 import 'package:awesome_music_rebased/utils/constants.dart';
 import 'package:awesome_music_rebased/views/home/tabs/discover.dart';
+import 'package:awesome_music_rebased/views/home/tabs/downloads.dart';
 import 'package:awesome_music_rebased/views/home/tabs/search.dart';
 import 'package:awesome_music_rebased/widgets/mini_player.dart';
 import 'package:flutter/material.dart';
@@ -12,60 +13,61 @@ class HomeScreen extends GetView<AppController> {
   @override
   Widget build(BuildContext context) {
     return Obx(
-      () => Scaffold(
-        backgroundColor: Colors.white,
-        bottomSheet: Get.find<SongController>().showMiniPlayer
-            ? const MiniPlayer()
-            : null,
-        bottomNavigationBar: BottomNavigationBar(
-          elevation: 10,
+      () {
+        final songController = Get.find<SongController>();
+        debugPrint(
+          'Value of ShowMiniPlayer is ${songController.showMiniPlayer}',
+        );
+        return Scaffold(
           backgroundColor: Colors.white,
-          currentIndex: controller.currentIndex,
-          selectedItemColor: colorBrandPrimaryLight,
-          unselectedItemColor: kSecondaryColor,
-          showSelectedLabels: true,
-          showUnselectedLabels: false,
-          onTap: controller.setCurrentIndex,
-          items: const [
-            BottomNavigationBarItem(
-              icon: Icon(Icons.music_note_outlined),
-              label: 'Explore',
-            ),
-            BottomNavigationBarItem(
-              icon: Icon(Icons.search),
-              label: 'Search',
-            ),
-            BottomNavigationBarItem(
-              icon: Icon(Icons.playlist_play_rounded),
-              label: 'Playlist',
-            ),
-            BottomNavigationBarItem(
-              icon: Icon(Icons.download_outlined),
-              label: 'Downloads',
-            ),
-          ],
-        ),
-        // appBar: CustAppBar(
-        //   title: controller.appTitle,
-        //   showBackButton: false,
-        // ),
-        body: Padding(
-          padding: EdgeInsets.only(
-            bottom: Get.find<SongController>().isPlaying ? 71 : 0,
-          ),
-          child: PageView(
-            physics: const NeverScrollableScrollPhysics(),
-            controller: controller.pageController,
-            onPageChanged: controller.setCurrentIndex,
-            children: const [
-              DiscoverScreen(),
-              SearchScreen(),
-              DiscoverScreen(),
-              DiscoverScreen(),
+          bottomSheet:
+              songController.showMiniPlayer ? const MiniPlayer() : null,
+          bottomNavigationBar: BottomNavigationBar(
+            elevation: 10,
+            backgroundColor: Colors.white,
+            currentIndex: controller.currentIndex,
+            selectedItemColor: colorBrandPrimaryLight,
+            unselectedItemColor: kSecondaryColor,
+            showSelectedLabels: true,
+            showUnselectedLabels: false,
+            onTap: (index) => controller.currentIndex = index,
+            items: const [
+              BottomNavigationBarItem(
+                icon: Icon(Icons.music_note_outlined),
+                label: 'Explore',
+              ),
+              BottomNavigationBarItem(
+                icon: Icon(Icons.search),
+                label: 'Search',
+              ),
+              BottomNavigationBarItem(
+                icon: Icon(Icons.playlist_play_rounded),
+                label: 'Playlist',
+              ),
+              BottomNavigationBarItem(
+                icon: Icon(Icons.download_outlined),
+                label: 'Downloads',
+              ),
             ],
           ),
-        ),
-      ),
+          body: Padding(
+            padding: EdgeInsets.only(
+              bottom: Get.find<SongController>().isPlaying ? 71 : 0,
+            ),
+            child: PageView(
+              physics: const NeverScrollableScrollPhysics(),
+              controller: controller.pageController,
+              onPageChanged: (index) => controller.currentIndex = index,
+              children: const [
+                DiscoverScreen(),
+                SearchScreen(),
+                SizedBox.shrink(),
+                DownloadsScreen(),
+              ],
+            ),
+          ),
+        );
+      },
     );
   }
 }
